b54f653dc0444685c88e3f03381c2ad775bfc11f
Muyang Niu <63112565+Muyang-Niu@users.noreply.github.com>
2020-10-07 14:10:15 +0800

    Add files via upload

---
+++ Chat Bot One.py
@@ -0,0 +1,130 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import nltk
+
+
+# In[4]:
+
+import io
+import random
+import string 
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[5]:
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) 
+
+
+# In[6]:
+
+f=open('chatbot.txt','r',errors = 'ignore')
+raw=f.read()
+raw = raw.lower()# change every word into a lower case.
+
+
+# In[7]:
+
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+
+# In[8]:
+
+sent_tokens
+
+
+# In[9]:
+
+word_tokens
+
+
+# In[10]:
+
+lemmer = nltk.stem.WordNetLemmatizer()
+#WordNet is a semantically-oriented dictionary of English included in NLTK.
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# return [lemmer.lemmatize(token) for token in tokens]
+# 意思就是对于每一个token在tokens里的，都拿出来lemmatize一下，放到lemmer这个list里边
+
+# In[11]:
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+def greeting(sentence):
+ 
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# In[12]:
+
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+
+# In[13]:
+
+flag=True
+print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("ROBO: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("ROBO: "+greeting(user_response))
+            else:
+                print("ROBO: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
